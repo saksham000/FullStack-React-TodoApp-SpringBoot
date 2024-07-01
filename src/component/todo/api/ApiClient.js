2f0ede14dc45560a180e92998f489d2f9556347f
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-07-01 22:29:52 +0530

    Optimised code for AWS

--- src/component/todo/api/ApiClient.js
+++ src/component/todo/api/ApiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://todo-app-backend-env.eba-yefepmj3.ap-south-1.elasticbeanstalk.com",
 });
